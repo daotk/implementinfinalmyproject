10896205f4f77439ce5fcd37320ce63dc64715e5
zintaen@gmail.com <zintaen@gmail.com@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-05 11:00:33 +0000

    Fixed bugs, now can work smoothly

--- trunk/Ehealth_System/DO/BaoCao/CreateBill_DO.cs
+++ trunk/Ehealth_System/DO/BaoCao/CreateBill_DO.cs
@@ -10,9 +10,10 @@ namespace DO.BaoCao
     public class CreateBill_DO
     {
         public string _BILLID { set; get; }
-        public string _PATIENTID { set; get; }
-        public string _USERID { set; get; }
-        public string _DESKID { set; get; }
+        public string _PATIENTNAME { set; get; }
+        public string _USERNAME { set; get; }
+        public string _PATIENTAGE { set; get; }
+        public string _PATIENTGENDER { set; get; }
         public string _SERVICEGROUPNAME { set; get; }
         public DateTime _BILLDATE { set; get; }
         public string _BILLCOST { set; get; }
