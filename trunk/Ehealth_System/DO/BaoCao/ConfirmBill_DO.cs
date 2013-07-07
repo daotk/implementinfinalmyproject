311fd771402e98f706824c539ffcc1a419958b47
huyngo233 <huyngo233@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-07 15:15:25 +0000

    ConfirmBill_DO

---
+++ trunk/Ehealth_System/DO/BaoCao/ConfirmBill_DO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO.BaoCao
+{
+   public class ConfirmBill_DO
+    {
+        public string _BILLID { set; get; }
+        public string _PATIENTNAME { set; get; }
+        public string _USERNAME { set; get; }
+        public string _PATIENTAGE { set; get; }
+        public string _PATIENTGENDER { set; get; }
+        public string _SERVICEGROUPNAME { set; get; }
+        public DateTime _BILLDATE { set; get; }
+        public string _BILLCOST { set; get; }
+        public bool _BILLSTATUS { set; get; }
+    }
+}
