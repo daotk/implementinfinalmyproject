a1e904b1c7e09b25716f93075b7b83f46d93b64f
huyngo233 <huyngo233@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-07 15:14:47 +0000

    confirmbill_bl

---
+++ Ehealth_System/BL/BaoCao/ConfirmBill_BL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DA.BaoCao;
+using DO.BaoCao;
+using DO.QuanTriHeThong;
+using DA.QuanTriHeThong;
+
+namespace BL.BaoCao
+{
+   public class ConfirmBill_BL
+    {
+        public List<CreateBill_DO> GetAllBill()
+        {
+            return CreateBill_DA.GetAllBills();
+        }
+
+        public List<CreateBill_DO> GetBillsByDay(DateTime time, string userid)
+        {
+            return CreateBill_DA.GetBillsByDay(time, userid);
+        }
+
+        public List<CreateBill_DO> GetBillsByWeek(DateTime fromtime, DateTime totime, string userid)
+        {
+            return CreateBill_DA.GetBillsByWeek(fromtime, totime, userid);
+        }
+
+        public List<CreateBill_DO> GetBillsByMonth(string month, string userid)
+        {
+            return CreateBill_DA.GetBillsByMonth(month, userid);
+        }
+
+        public List<User_DO> GetAllUser()
+        {
+            return CreateBill_DA.GetAllUsers();
+        }
+    }
+}
