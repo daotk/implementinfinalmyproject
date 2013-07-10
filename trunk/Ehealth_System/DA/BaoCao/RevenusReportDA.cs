08fafac1e0cc7a6d116bf193adb08b220fb5981a
thienphuta1907 <thienphuta1907@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-10 03:24:57 +0000

    update bao cao

--- trunk/Ehealth_System/DA/BaoCao/RevenusReportDA.cs
+++ trunk/Ehealth_System/DA/BaoCao/RevenusReportDA.cs
@@ -20,6 +20,9 @@ namespace DA.BaoCao
                     us.tendichvu_ = row.SERVICEGROUPNAME;
                     dsusergroup.Add(us);
                 }
+                LoaiDichVuDO us1 = new LoaiDichVuDO();
+                us1.tendichvu_ = "Tất cả loại dịch vụ";
+                dsusergroup.Add(us1);
             }
             return dsusergroup;
         }
@@ -52,6 +55,7 @@ namespace DA.BaoCao
                             join p in dk.DeskCashiers on u.DESKID equals p.DESKID
                             join k in dk.Patient_Info on u.PATIENTID equals k.PATIENTID
                             where u.SERVICEGROUPNAME == tenloaidichvu
+                                  && u.BILLSTATUS == true
                                   && p.Department_Info.DEPARTMENTNAME == tendonvithungan
                                   && u.BILLDATE.Day == ngay.Day 
                                   && u.BILLDATE.Month == ngay.Month
@@ -86,7 +90,7 @@ namespace DA.BaoCao
                             join k in dk.Patient_Info on u.PATIENTID equals k.PATIENTID
                             where u.SERVICEGROUPNAME == tenloaidichvu
                                   && p.Department_Info.DEPARTMENTNAME == tendonvithungan
-                                
+                                && u.BILLSTATUS == true
                                  && u.BILLDATE.Month == ngay.Month
                                 
 
@@ -108,6 +112,71 @@ namespace DA.BaoCao
             return dsusergroup;
         }
 
-        
+        public static List<thongtinbaocaoDO> GetDonViThuNganTheoNgayAll(string tenloaidichvu, string tendonvithungan
+            , DateTime ngay)
+        {
+            List<thongtinbaocaoDO> dsusergroup = new List<thongtinbaocaoDO>();
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.Bill_Info
+                            join p in dk.DeskCashiers on u.DESKID equals p.DESKID
+                            join k in dk.Patient_Info on u.PATIENTID equals k.PATIENTID
+                            where 
+                                   u.BILLSTATUS == true
+                                  && p.Department_Info.DEPARTMENTNAME == tendonvithungan
+                                  && u.BILLDATE.Day == ngay.Day
+                                  && u.BILLDATE.Month == ngay.Month
+                                  && u.BILLDATE.Year == ngay.Year
+
+                            select u;
+                foreach (var row in query)
+                {
+                    thongtinbaocaoDO us = new thongtinbaocaoDO();
+                    us.tendonvithungan_ = row.DeskCashier.Department_Info.DEPARTMENTNAME;
+                    us.mabienlai_ = row.BILLID;
+                    us.hotenbenhnhan_ = row.Patient_Info.PATIENTNAME;
+                    us.tuoibenhnhan_ = row.Patient_Info.AGE;
+                    us.gioitinh_ = row.Patient_Info.GENDER;
+                    us.ngaydangky_ = row.BILLDATE;
+                    us.tongtien_ = row.BILLCOST;
+                    us.tennhomdichvu_ = row.SERVICEGROUPNAME;
+                    dsusergroup.Add(us);
+                }
+            }
+            return dsusergroup;
+        }
+        public static List<thongtinbaocaoDO> GetDonViThuNganTheoThangAll(string tenloaidichvu, string tendonvithungan
+            , DateTime ngay)
+        {
+            List<thongtinbaocaoDO> dsusergroup = new List<thongtinbaocaoDO>();
+            using (Entity.EHealthSystemEntities dk = new Entity.EHealthSystemEntities())
+            {
+                var query = from u in dk.Bill_Info
+                            join p in dk.DeskCashiers on u.DESKID equals p.DESKID
+                            join k in dk.Patient_Info on u.PATIENTID equals k.PATIENTID
+                            where 
+                                   p.Department_Info.DEPARTMENTNAME == tendonvithungan
+                                && u.BILLSTATUS == true
+                                 && u.BILLDATE.Month == ngay.Month
+
+
+                            select u;
+                foreach (var row in query)
+                {
+                    thongtinbaocaoDO us = new thongtinbaocaoDO();
+                    us.tendonvithungan_ = row.DeskCashier.Department_Info.DEPARTMENTNAME;
+                    us.mabienlai_ = row.BILLID;
+                    us.hotenbenhnhan_ = row.Patient_Info.PATIENTNAME;
+                    us.tuoibenhnhan_ = row.Patient_Info.AGE;
+                    us.gioitinh_ = row.Patient_Info.GENDER;
+                    us.ngaydangky_ = row.BILLDATE;
+                    us.tongtien_ = row.BILLCOST;
+                    us.tennhomdichvu_ = row.SERVICEGROUPNAME;
+                    dsusergroup.Add(us);
+                }
+            }
+            return dsusergroup;
+        }
+
     }
 }
