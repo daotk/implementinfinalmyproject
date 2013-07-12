666e780663b346343f08fd8f2dd0665b4e524cbf
daokhau1 <daokhau1@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-12 03:43:07 +0000

    update DO

---
+++ trunk/Ehealth_System/DO/BigScreen_DO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class BigScreen_DO
+    {
+        public string _MaBN { set; get; }
+        public string _TenBN { set; get; }
+        public string _TongTien { set; get; }
+    }
+}
