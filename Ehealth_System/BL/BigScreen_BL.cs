fa7df63a5d8b8ee5f5641e386ac7cbf63e14960f
daokhau1 <daokhau1@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-12 03:43:24 +0000

    update BL

---
+++ Ehealth_System/BL/BigScreen_BL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DO;
+using DA;
+
+namespace BL
+{
+    public class BigScreen_BL
+    {
+        public static List<DO.BigScreen_DO> Get()
+        {
+            return DA.BigScreen_DA.Get();
+        }
+    }
+}
