eabb99adb17dcf913368b4714d9c2ec8f280799e
daokhau1 <daokhau1@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-12 03:42:47 +0000

    update GUi

---
+++ Ehealth_System/GUI/BigScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DA.Entity;
+
+
+namespace GUI
+{
+    public partial class BigScreen : Form
+    {
+       
+        public BigScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void BigScreen_Load(object sender, EventArgs e)
+        {
+            grd_Thongtin.DataSource = BL.BigScreen_BL.Get();
+            
+        }
+    }
+}
