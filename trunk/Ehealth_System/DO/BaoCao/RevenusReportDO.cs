af02c91126ebda46e45396fdbce228af82344cda
thienphuta1907 <thienphuta1907@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-15 02:53:02 +0000

    update

--- trunk/Ehealth_System/DO/BaoCao/RevenusReportDO.cs
+++ trunk/Ehealth_System/DO/BaoCao/RevenusReportDO.cs
@@ -18,12 +18,8 @@ namespace DO.BaoCao
     public class thongtinbaocaoDO
     {
         public string tendonvithungan_ { get; set; }
-        public string mabienlai_ { get; set; }
-        public string hotenbenhnhan_ { get; set; }
-        public string tuoibenhnhan_ { get; set; }
-        public string gioitinh_ { get; set; }
         public DateTime ngaydangky_ { get; set; }
         public string tongtien_ { get; set; }
-        public string tennhomdichvu_ { get; set; }
+        
     }
 }
