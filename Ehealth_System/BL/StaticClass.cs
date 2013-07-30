b305f57c8bec90da612986acf666d1ee921ec1e0
daokhau1 <daokhau1@039d09b2-d945-36a4-8aca-1cebad7ffd88>
2013-07-30 02:37:20 +0000

    update Static class

--- Ehealth_System/BL/StaticClass.cs
+++ Ehealth_System/BL/StaticClass.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.ComponentModel;
 
 namespace BL
 {
@@ -19,6 +21,23 @@ namespace BL
         public static string tenbanthungan;
         public static bool? Online;
         public static bool? OpenBigScreen = false;
+        public static DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            PropertyDescriptorCollection properties =
+               TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+
+        }
 
     }
 }
